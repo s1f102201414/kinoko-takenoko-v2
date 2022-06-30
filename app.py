2420fc2aaf3eda21b45efbe3051a1dd3ce327347
Shuta Takaoka <s1f102201414@iniad.org>
2022-06-30 16:15:46 +0900

    11-2

--- app.py
+++ app.py
@@ -1,12 +1,25 @@
 from flask import Flask, render_template, request
 app = Flask(__name__)
 
+kinoko_count = 3
+takenoko_count = 5
+messages=['Hit1','Hit2']
+
 @app.route('/')
 def top():
     return render_template('index.html', **vars())
 
 @app.route('/vote', methods=['POST'])
 def answer():
+    # 投票による更新
+    global kinoko_count, takenoko_count, messages
+    if request.form.get('item')=='kinoko':
+        kinoko_count += 1
+    elif request.form.get('item')=='takenoko':
+        takenoko_count += 1
+    #進歩グラフの追加
+    kinoko_percent = kinoko_count / (kinoko_count + takenoko_count)* 100
+    takenoko_percent = takenoko_count / (kinoko_count + takenoko_count) * 100
     return render_template('vote.html', **vars())
 
 if __name__ == '__main__':
